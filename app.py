2221c58770dc1f8e1b5a24d19ffdc97cfc417c20
oehimen1 <candaceehimen@gmail.com>
2024-05-06 12:09:28 -0400

    Python Tutorial

---
+++ app.py
@@ -0,0 +1,38 @@
+# print('Obehi Ehimen')
+
+# How python code gets executed: line by line from the top
+# print('o----') # string - an array of characters
+# print(' ||||')
+# print('*' * 10) # this expression prints the start 10 times
+#
+# # Variables - to temporarily store data in a computer's memory
+# price = 10 # integer - number with out a decimal point
+# rating = 4.9 # decimal or floating point number
+# name = 'Mosh'
+# is_published = True # True of False not true or false
+# print('This is the price of quality cheese:',price)
+#
+# name = 'John Smith'
+# age = 20
+# is_new_patient = True
+
+#Receiving Input
+
+# name = input('What is your name? ')
+# color = input('What is your favorite color? ')
+# print(name + ' likes ' + color) #concatenating
+
+# Type Conversion
+# birth_year = input('Birth Year: ')
+# print(type(birth_year))
+# age = 2024 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# weight_lbs = input('What is your weight (lbs)? ')
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+
+# Strings
+# course = 'Python for "Beginners"'
+# print(course)
