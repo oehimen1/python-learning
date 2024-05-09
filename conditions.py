d541765088db9e97b7ce82c57606d739e1ba43ae
oehimen1 <candaceehimen@gmail.com>
2024-05-09 09:22:22 -0400

    finished conditions; starting mini project

--- conditions.py
+++ conditions.py
@@ -23,3 +23,40 @@ else:
 print(f"Your down payment is: ${down_payment}")
 
 # Logical Operators
+print('-' * 20)
+has_good_credit = True
+has_high_income = False
+has_criminal_record = False
+
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+
+if has_high_income or has_good_credit:
+    print("Eligible for loan")
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
+
+# AND: both conditions need to be true
+# OR: at least one conditon should be true
+# NOT: inverts any boolean value given
+
+
+#Comparison Operators
+print('-' * 20)
+
+temperature = 40
+if temperature > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+# '=' is an assignment statement'; '==' is a comparison statement producing a boolean value
+
+name = input("What is your name? ")
+if len(name) < 3:
+    print("Name must be at least 3 characters")
+elif len(name) > 50:
+    print("Name must be a a maximum of 50 characters")
+else:
+    print("Name looks good!")
---
+++ miniProjects/weight_converter.py
@@ -0,0 +1,3 @@
+weight = input("Please enter your weight: ")
+lb_or_kg = input("(L)bs or (K)g: ")
+
